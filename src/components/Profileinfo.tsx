df3d59d5ca82fe4422721660cebcfae9e0a1efcf
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-02-19 18:52:39 +0100

    profileInfo initial layout

--- src/components/Profileinfo.tsx
+++ src/components/Profileinfo.tsx
@@ -2,11 +2,18 @@ import { RootState, useAppSelector } from "../redux/store";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserAction } from "../redux/actions";
+import { Col, Row } from "react-bootstrap";
 
 export const Profileinfo = () => {
 	const dispatch = useDispatch();
 	const profileInfo = useAppSelector((state) => state.profile.me);
 
-	dispatch(getUserAction("me"));
-	return <div>Profileinfo</div>;
+	// dispatch(getUserAction("me"));
+	return (
+		<Row className="flex-column">
+			<Col></Col>
+			<Col></Col>
+			<Col></Col>
+		</Row>
+	);
 };
