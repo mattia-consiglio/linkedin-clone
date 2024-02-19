8e74efac09cd67c46899201046fcfc8f087e3f46
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-02-19 21:08:57 +0100

    account section - last middle section and some mobile css are missing

--- src/components/Profileinfo.tsx
+++ src/components/Profileinfo.tsx
@@ -15,15 +15,21 @@ export const Profileinfo = () => {
 		<Row className="flex-column justify-content-center align-items-between">
 			<Col>
 				<img
-					className="rounded-top-3 w-100"
-					src="https://placekitten.com/200/50"
-					alt="gatto bg"
+					className="rounded-top-3 w-100 z-1"
+					src="https://placekitten.com/200/56"
+					alt="bg profilo"
 				/>
 			</Col>
 			<Col>
-				<Row className="justify-content-center">
-					<Col>Ciao</Col>
-					<Col>Ciao</Col>
+				<Row className="justify-content-between">
+					<Col>
+						<img
+							className="fotoProfilo ms-4 rounded-circle border border-white border-4 z-4"
+							src="https://placedog.net/150/150"
+							alt="foto profilo"
+						/>
+					</Col>
+					<Col className="text-end me-3 mt-3">Biro</Col>
 				</Row>
 				<Row className="justify-content-center">
 					<Col>Ciao</Col>
@@ -36,9 +42,37 @@ export const Profileinfo = () => {
 				</Row>
 			</Col>
 			<Col>
-				<Row>
-					<Col>
-						<div className="text-center bg-info rounded-3 mb-2">Ciao</div>
+				<Row className="justify-content-evenly">
+					<Col className="bgServiziOfferti rounded-2 mb-2" xs={5}>
+						<Row className="flex-column justify-content-start my-3">
+							<Col>
+								<Row className="justify-content-between">
+									<Col className="fw-bold">Servizi offerti</Col>
+									<Col className="text-end">biro</Col>
+								</Row>
+							</Col>
+							<Col>Sviluppare il mondo ...</Col>
+							<Col className="text-primary fw-bold">Mostra dettagli</Col>
+						</Row>
+					</Col>
+					<Col
+						className="border bgServiziOf rounded-2 mb-2 d-none d-md-inline"
+						xs={5}
+					>
+						<Row className="flex-column justify-content-start my-3">
+							<Col>
+								<Row className="justify-content-between">
+									<Col xs={10} className="fw-bold">
+										Mostra ai recruiter che sei disponibile a lavorare :
+									</Col>
+									<Col xs={1} className="text-end">
+										X
+									</Col>
+								</Row>
+							</Col>
+							<Col>sei tu che decidi chi può vedere questa informazione.</Col>
+							<Col className="text-primary fw-bold">Inizia</Col>
+						</Row>
 					</Col>
 				</Row>
 			</Col>
