df8794cee1c03f6b625cccae1a73739f99126b8a
Mattia Consiglio <mattiaconsiglio96@gmail.com>
2024-02-19 18:51:54 +0100

    Fixed Profileinfo.tsx

--- src/components/Profileinfo.tsx
+++ src/components/Profileinfo.tsx
@@ -1,12 +1,14 @@
 import { RootState, useAppDispatch, useAppSelector } from "../redux/store";
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserAction } from "../redux/actions";
 
 export const Profileinfo = () => {
 	const dispatch = useAppDispatch();
 	const profileInfo = useAppSelector((state: RootState) => state.profile.me);
+	useEffect(() => {
+		dispatch(getUserAction("me"));
+	}, []);
 
-	dispatch(getUserAction("me"));
 	return <div>Profileinfo</div>;
 };
