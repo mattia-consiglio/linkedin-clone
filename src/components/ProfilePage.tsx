7bdb277d21a9b2081b1cb3367961151ab286dfc4
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-02-19 15:05:40 +0100

    ProfilePage GridSistem

---
+++ src/components/ProfilePage.tsx
@@ -0,0 +1,30 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const Profile = () => {
+	return (
+		<Container>
+			<Row className="flex-column flex-md-row justify-content-center align-items-top">
+				<Col xs={12} md={8}>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi,
+					voluptatum laborum minus autem sit placeat aut iusto iure officia
+					dignissimos quasi possimus tempora velit debitis expedita suscipit
+					animi quam aperiam? Lorem, ipsum dolor sit amet consectetur
+					adipisicing elit. Voluptatum, libero officiis. Corrupti, perferendis
+					explicabo quidem cupiditate doloremque unde eos expedita, veniam iusto
+					eligendi eum! Eos sunt nobis alias ratione ut? Lorem, ipsum dolor sit
+					amet consectetur adipisicing elit. Et repudiandae incidunt obcaecati
+					iusto illum, distinctio laudantium sint maxime, tenetur quod dolores?
+					Officiis nemo ducimus aliquam iste dolorem laudantium fugiat tenetur?
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
+					non magni quae nostrum nihil itaque rem, ab fugit sit dolor explicabo
+					repellat recusandae, doloribus fuga ex asperiores ullam quam delectus?
+				</Col>
+				<Col xs={12} md={4}>
+					PROVA
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+export default Profile;
