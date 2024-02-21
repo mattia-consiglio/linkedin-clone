7ad41d3895ab9feaa0dad6d633049ef569617e2b
Ciro Vitiello <ciro.riccione@gmail.com>
2024-02-21 17:11:21 +0100

    homepage finished

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profile from "./components/ProfilePage";
 import NavBar from "./components/NavBar";
 import ExperiencesPage from "./components/ExperiencesPage";
+import SideProfile from "./components/SideProfile";
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 				</header>
 				<main className="BgPage">
 					<Routes>
+						<Route path="/" element={<SideProfile />}></Route>
 						<Route path="/profile" element={<Profile />}></Route>
 						<Route path="/experiences" element={<ExperiencesPage />}></Route>
 					</Routes>
---
+++ src/components/News.tsx
@@ -0,0 +1,63 @@
+import { Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+const News = () => {
+	return (
+		<>
+			<Container>
+				<Row className="justify-content-center">
+					<Col xs={12} md={3} className="my-2">
+						<Card>
+							<Card.Title className=" px-3 pt-4 fs-6 d-flex justify-content-between">
+								LinkedIn Notizie{" "}
+								<span>
+									<i className="bi bi-info-square-fill"></i>
+								</span>
+							</Card.Title>
+							<Card.Body className="ps-1 pe-3">
+								<ul className="">
+									<li>
+										<h6 className=" text-truncate ">
+											Verso una pubblica amministrazioneeeeeeeeeeeee
+										</h6>
+										<p>19 ore fa</p>
+									</li>
+									<li>
+										<h6 className=" text-truncate ">
+											Come se la passano gli orsi polari
+										</h6>
+										<p>1 giorno fa</p>
+									</li>
+									<li>
+										<h6 className=" text-truncate ">
+											Disastro in JS con querySelector
+										</h6>
+										<p>1 giorno fa</p>
+									</li>
+									<li>
+										<h6 className=" text-truncate ">
+											Lo sviluppo del frontender alpha
+										</h6>
+										<p>17 ore fa</p>
+									</li>
+									<li>
+										<h6 className=" text-truncate ">
+											Di sicuro non farò queste sezioni in azienda
+										</h6>
+										<p>4 giorni fa</p>
+									</li>
+								</ul>
+							</Card.Body>
+
+							<Button className=" text-start ps-md-5 py-3 fw-semibold border-top border border-0 customButton w-100">
+								Vedi altro
+								{/*  mattia prendi l'icona da linkedIN originale pls  */}
+							</Button>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
+};
+export default News;
